a2acc7e5ee48ec3221c7a3296d6f4609f9984d88
Arvia <arviaglass@Arvias-Cube.local>
2014-11-07 11:55:44 -0800

    root change again

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ root to: "students#index"
 resources :students
 resources :projects
 
-
+#
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
